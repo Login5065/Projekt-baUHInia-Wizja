66749b886cb6776c939ba8ed188c19862b561319
Ciachociech <bastanator.rex@gmail.com>
2020-12-07 22:15:00 +0100

    Adding classes as C# scripts (without implementation and integration with other modules)

---
+++ Assets/Scripts/HeatModule/MapHeat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapHeat : MonoBehaviour
+{
+    private float temperature { get; set; }
+    private float heatModifier { get; set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        temperature = 20.0f;
+        heatModifier = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float CalculateTemperature(Tile[] tiles)
+    {
+        //List<Building> allBuildings;
+        return 0.0f;
+    }
+
+}

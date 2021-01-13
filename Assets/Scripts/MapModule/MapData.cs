5a89cb30ab3b9b30000f02924489fb1f2af71001
Purban <xpurbanx@wp.pl>
2021-01-13 08:57:25 +0100

    GameData merged JSON container

--- Assets/Scripts/MapModule/MapData.cs
+++ Assets/Scripts/MapModule/MapData.cs
@@ -6,10 +6,9 @@ using UnityEngine;
 [System.Serializable]
 public class MapData
 {
-    public string mapName;
     public int budget;
     public int columnsNumber;
     public int rowsNumber;
     public Tile[] tiles;
-    public MapHeat mapHeat;
+    //public MapHeat mapHeat;
 }

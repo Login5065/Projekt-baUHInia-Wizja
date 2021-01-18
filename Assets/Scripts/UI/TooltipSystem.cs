1181202fd300401701d8becfe13091c4bd0e45c0
Purban <xpurbanx@wp.pl>
2021-01-18 01:34:30 +0100

    Tooltip system

---
+++ Assets/Scripts/UI/TooltipSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipSystem : MonoBehaviour
+{
+    private static TooltipSystem Instance;
+
+    public Tooltip tooltip;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public static void Show(string content, string header = "")
+    {
+        Instance.tooltip.SetText(content, header);
+        Instance.tooltip.gameObject.SetActive(true);
+    }
+
+    public static void Hide()
+    {
+        Instance.tooltip.gameObject.SetActive(false);
+
+    }
+
+
+}
